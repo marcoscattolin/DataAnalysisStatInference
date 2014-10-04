40bde22736967990f011ee4f66d85f84753e5d5b
marcoscattolin <marco.scattolin@gmail.com>
2014-10-04 11:02:42 +0200

    Recap questions for assignment
    Copedi questions to draft RMD file

--- preliminary.R
+++ preliminary.R
@@ -1,10 +1,21 @@
+library(dplyr)
 load(url("http://bit.ly/dasi_gss_data"))
 
 
 summary(gss)
 
-selected <- gss[,c("degree","natenvir")]
-mosaicplot(selected$degree ~ selected$natenvir)
+selected <- gss[,c("degree","tvhours")]
+boxplot(selected$tvhours ~ selected$degree)
 
-Q:  Is there a relationship between the highest school degree achieved
-and the unmber of tv hours watched per day?
+selected <- group_by(selected,degree)
+
+
+par(mfrow = c(2,1))
+hist(selected$tvhours,main = "Distribution of variable 'tvhours'", xlab="TV hours watched on average day")
+barplot(table(selected$degree), main = "Distribution of variable 'degree'")
+
+
+mean(selected$tvhours,na.rm = T)
+sd(selected$tvhours,na.rm = T)
+
+write.csv(selected,"selected.csv")
