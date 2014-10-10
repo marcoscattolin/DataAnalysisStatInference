93397e7788767c3eb844b255423f9c2c504bcf37
marcoscattolin <marco.scattolin@gmail.com>
2014-10-10 10:52:16 +0200

    New partial drop

--- preliminary.R
+++ preliminary.R
@@ -1,21 +1,11 @@
 library(dplyr)
+source("http://bit.ly/dasi_inference")
 load(url("http://bit.ly/dasi_gss_data"))
 
-
-summary(gss)
-
-selected <- gss[,c("degree","tvhours")]
-boxplot(selected$tvhours ~ selected$degree)
-
-selected <- group_by(selected,degree)
+gss <- tbl_df(gss)
+gss <- select(gss, degree, tvhours) %>% filter(!is.na(degree) & !is.na(tvhours)) %>% group_by(degree)
+a <- summarise(gss,num = n(), mean = mean(tvhours),sd = sd(tvhours))
 
 
-par(mfrow = c(2,1))
-hist(selected$tvhours,main = "Distribution of variable 'tvhours'", xlab="TV hours watched on average day")
-barplot(table(selected$degree), main = "Distribution of variable 'degree'")
-
-
-mean(selected$tvhours,na.rm = T)
-sd(selected$tvhours,na.rm = T)
-
-write.csv(selected,"selected.csv")
+mod <- lm(gss$tvhours~gss$degree)
+aa <- anova(mod)
